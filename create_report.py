530fac4079f0b5bda1478bf253a0fb4902552548
eseblasan <liadenko.ik@gmail.com>
2026-05-06 19:12:00 +0300

    Add: Centering text in an Excel file and automatically adjusting it to the cell text size

--- create_report.py
+++ create_report.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from datetime import date
 from openpyxl import load_workbook
-from openpyxl.styles import Font
+from openpyxl.styles import Font, Alignment
+from openpyxl.utils import get_column_letter
 
 
 def add_link_to_data(group_name: str, link: str, data) -> None:
@@ -20,15 +21,20 @@ def create_report(data: dict, file_path: str) -> str:
     wb = load_workbook(file_path)
     ws = wb.active
 
+
+    center_alignment = Alignment(horizontal="center", vertical="center")
+
     for row in ws.iter_rows():
         for i, cell in enumerate(row):
+
+
+            cell.alignment = center_alignment
 
             if i == 0:
                 continue
 
             if cell.value:
                 url = str(cell.value).strip()
-
                 if not url.startswith("http"):
                     url = "https://" + url
 
@@ -36,20 +42,19 @@ def create_report(data: dict, file_path: str) -> str:
                 cell.hyperlink = url
                 cell.font = Font(color="0000FF", underline="single")
 
+    for col in ws.columns:
+        max_length = 0
+        column_letter = get_column_letter(col[0].column)
+
+        for cell in col:
+            try:
+                if len(str(cell.value)) > max_length:
+                    max_length = len(str(cell.value))
+            except:
+                pass
+
+        adjusted_width = (max_length + 4)
+        ws.column_dimensions[column_letter].width = adjusted_width
+
     wb.save(file_path)
     return file_path
-
-
-if __name__ == "__main__":
-    group_name = ""
-    links = []
-    data = {
-        group_name: []
-    }
-
-    for link in links:
-        add_link_to_data(group_name, link, data)
-
-    test_file_name = "report_" + date.today().strftime("%d-%m-%Y") + ".xlsx"
-    create_report(data, test_file_name)
-    print(f"Тестовый файл {test_file_name} успешно создан!")
